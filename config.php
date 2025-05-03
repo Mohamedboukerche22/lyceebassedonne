5ddb264e74c72e84cd96f564d09e984d73b5dd4a
Mohamedboukerche22 <mohamedboukerche55@gmail.com>
2025-05-03 01:17:01 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 
-$host = 'sql313.hstn.me';
-$dbname = 'mseet_38835548_school_management';
-$username = 'mseet_38835548';
-$password = 'X7hNwHyfKrJo';
+$host = '';
+$dbname = 'basee';
+$username = 'root';
+$password = 'password';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
